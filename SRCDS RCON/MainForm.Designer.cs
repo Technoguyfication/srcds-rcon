210eebe7279456636976ea37a0549b90701101e6
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-10 15:29:49 -0700

    set console log to readonly

--- SRCDS RCON/MainForm.Designer.cs
+++ SRCDS RCON/MainForm.Designer.cs
@@ -169,6 +169,7 @@
 			this.consoleTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.consoleTextBox.Location = new System.Drawing.Point(0, 0);
 			this.consoleTextBox.Name = "consoleTextBox";
+			this.consoleTextBox.ReadOnly = true;
 			this.consoleTextBox.Size = new System.Drawing.Size(339, 200);
 			this.consoleTextBox.TabIndex = 0;
 			this.consoleTextBox.Text = "Lorem ipsum\ndolor sit amet";
