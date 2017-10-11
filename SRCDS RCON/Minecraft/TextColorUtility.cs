6447137b44cf3edbf568b13b214aff55f2ada8f3
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-11 01:03:15 -0700

    make some more classes

---
+++ SRCDS RCON/Minecraft/TextColorUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRCDS_RCON.Minecraft
+{
+	public static class TextColorUtility
+	{
+		/// <summary>
+		/// Returns a map of strings and their corresponding colors from a Bukkit console
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public static Dictionary<string, ConsoleColor> GetMappedColorStrings(string input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
