2dd788d909e0a5c70094aa2dc534fd5d7c3ad424
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-12 20:31:23 -0700

    start work on protocol

---
+++ SRCDS RCON/Net/Packet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRCDS_RCON.Net
+{
+	public class Packet
+	{
+		/// <summary>
+		/// The length of the entire packet, in bytes
+		/// </summary>
+		public int Length { get; }
+
+		/// <summary>
+		/// Used for matching requests to their responses, randomly generated for each packet
+		/// </summary>
+		public int PacketID
+		{
+			get
+			{
+				return new Random().Next();
+			}
+		}
+
+		/// <summary>
+		/// Type of the packet<para/>
+		/// See https://developer.valvesoftware.com/wiki/Source_RCON_Protocol#Packet_Type
+		/// </summary>
+		public PacketType Type { get; protected set; }
+
+		/// <summary>
+		/// Body of the packet
+		/// </summary>
+		public byte[] Body { get; protected set; }
+
+		/// <summary>
+		/// The padding at the end of the packet, basically a suffix.<para />
+		/// See https://developer.valvesoftware.com/wiki/Source_RCON_Protocol#Empty_String
+		/// </summary>
+		public byte[] Padding
+		{
+			get
+			{
+				return new byte[] { 0x00, 0x00 };
+			}
+		}
+
+		/// <summary>
+		/// Fully assembled packet, suitable for sending
+		/// </summary>
+		public byte[] Raw
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Defines packets sent to and from the server.<para/>
+	/// "RESPONSE" packets are sent from S->C, and vice-versa
+	/// </summary>
+	public enum PacketType : int
+	{
+		AUTH = 3,
+		AUTH_RESPONSE = 2,
+		COMMAND = 2,
+		COMMAND_RESPONSE = 0
+	}
+}
--- SRCDS RCON/Net/Protocol.cs
+++ SRCDS RCON/Net/Protocol.cs
@@ -3,11 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace SRCDS_RCON.Net
 {
-	public class Protocol
+	public class Protocol : IDisposable
 	{
+		private TcpClient _client;
+		private CancellationTokenSource _cancellationtokenSource;
+		private Task _listenTask;
 
+		/// <summary>
+		/// Occurs when the server sends the client a message, like the resposne to a comment
+		/// </summary>
+		public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+		/// <summary>
+		/// Occurs when the client is connected and ready to send data
+		/// </summary>
+		public event EventHandler<EventArgs> Ready;
+
+		/// <summary>
+		/// Occurs when the client is disconnected
+		/// </summary>
+		public event EventHandler<EventArgs> Stopped;
+
+		/// <summary>
+		/// Whether the client is connected or not
+		/// </summary>
+		public bool Connected
+		{
+			get
+			{
+				if (_client != null)
+				{
+					return _client.Connected;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		public Protocol()
+		{
+			
+		}
+
+		~Protocol()
+		{
+			Dispose();
+		}
+
+		public void Connect(Server server)
+		{
+			if (Connected)
+				Disconnect();
+
+			_cancellationtokenSource = new CancellationTokenSource();
+			
+
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			Disconnect();
+		}
+
+		public void Disconnect()
+		{
+			_cancellationtokenSource?.Cancel();
+			_client?.Close();
+		}
+
+		private void Listen()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void SendAuthentication()
+		{
+
+		}
+	}
+
+	public class MessageReceivedEventArgs : EventArgs
+	{
+		public string Message { get; set; }
+	}
+
+
+	[Serializable]
+	public class InvalidCredentialsException : Exception
+	{
+		public InvalidCredentialsException() { }
+		public InvalidCredentialsException(string message) : base(message) { }
+		public InvalidCredentialsException(string message, Exception inner) : base(message, inner) { }
+		protected InvalidCredentialsException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 	}
 }
