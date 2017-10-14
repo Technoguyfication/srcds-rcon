97a9c9a89cbeaf7666c0342405672f79b5d772cf
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-13 22:07:27 -0700

    start adding password box to server creation menu

--- SRCDS RCON.GUI/CreateServerForm.Designer.cs
+++ SRCDS RCON.GUI/CreateServerForm.Designer.cs
@@ -36,6 +36,8 @@
 			this.typeLabel = new System.Windows.Forms.Label();
 			this.cancelButton = new System.Windows.Forms.Button();
 			this.saveButton = new System.Windows.Forms.Button();
+			this.passwordLabel = new System.Windows.Forms.Label();
+			this.passwordTextBox = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// hostTextBox
@@ -44,35 +46,34 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.hostTextBox.Location = new System.Drawing.Point(50, 13);
 			this.hostTextBox.Name = "hostTextBox";
-			this.hostTextBox.Size = new System.Drawing.Size(177, 20);
+			this.hostTextBox.Size = new System.Drawing.Size(243, 20);
 			this.hostTextBox.TabIndex = 1;
 			// 
 			// portTextBox
 			// 
 			this.portTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.portTextBox.Location = new System.Drawing.Point(268, 13);
+			this.portTextBox.Location = new System.Drawing.Point(334, 13);
 			this.portTextBox.Name = "portTextBox";
-			this.portTextBox.Size = new System.Drawing.Size(62, 20);
+			this.portTextBox.Size = new System.Drawing.Size(76, 20);
 			this.portTextBox.TabIndex = 3;
 			// 
 			// typeComboBox
 			// 
-			this.typeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+			this.typeComboBox.Anchor = System.Windows.Forms.AnchorStyles.Right;
 			this.typeComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
 			this.typeComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
 			this.typeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.typeComboBox.FormattingEnabled = true;
-			this.typeComboBox.Location = new System.Drawing.Point(50, 40);
+			this.typeComboBox.Location = new System.Drawing.Point(313, 40);
 			this.typeComboBox.Name = "typeComboBox";
-			this.typeComboBox.Size = new System.Drawing.Size(131, 21);
+			this.typeComboBox.Size = new System.Drawing.Size(97, 21);
 			this.typeComboBox.TabIndex = 5;
 			// 
 			// portLabel
 			// 
 			this.portLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.portLabel.AutoSize = true;
-			this.portLabel.Location = new System.Drawing.Point(233, 16);
+			this.portLabel.Location = new System.Drawing.Point(299, 16);
 			this.portLabel.Name = "portLabel";
 			this.portLabel.Size = new System.Drawing.Size(29, 13);
 			this.portLabel.TabIndex = 2;
@@ -89,9 +90,9 @@
 			// 
 			// typeLabel
 			// 
-			this.typeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.typeLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
 			this.typeLabel.AutoSize = true;
-			this.typeLabel.Location = new System.Drawing.Point(12, 43);
+			this.typeLabel.Location = new System.Drawing.Point(275, 43);
 			this.typeLabel.Name = "typeLabel";
 			this.typeLabel.Size = new System.Drawing.Size(38, 13);
 			this.typeLabel.TabIndex = 4;
@@ -101,7 +102,7 @@
 			// 
 			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.Location = new System.Drawing.Point(187, 40);
+			this.cancelButton.Location = new System.Drawing.Point(267, 67);
 			this.cancelButton.Name = "cancelButton";
 			this.cancelButton.Size = new System.Drawing.Size(61, 23);
 			this.cancelButton.TabIndex = 6;
@@ -112,7 +113,7 @@
 			// saveButton
 			// 
 			this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.saveButton.Location = new System.Drawing.Point(254, 40);
+			this.saveButton.Location = new System.Drawing.Point(334, 67);
 			this.saveButton.Name = "saveButton";
 			this.saveButton.Size = new System.Drawing.Size(76, 23);
 			this.saveButton.TabIndex = 7;
@@ -120,20 +121,40 @@
 			this.saveButton.UseVisualStyleBackColor = true;
 			this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
 			// 
+			// passwordLabel
+			// 
+			this.passwordLabel.AutoSize = true;
+			this.passwordLabel.Location = new System.Drawing.Point(12, 43);
+			this.passwordLabel.Name = "passwordLabel";
+			this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+			this.passwordLabel.TabIndex = 0;
+			this.passwordLabel.Text = "Password:";
+			// 
+			// passwordTextBox
+			// 
+			this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.passwordTextBox.Location = new System.Drawing.Point(74, 40);
+			this.passwordTextBox.Name = "passwordTextBox";
+			this.passwordTextBox.Size = new System.Drawing.Size(195, 20);
+			this.passwordTextBox.TabIndex = 1;
+			// 
 			// CreateServerForm
 			// 
 			this.AcceptButton = this.saveButton;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(342, 75);
+			this.ClientSize = new System.Drawing.Size(422, 114);
 			this.Controls.Add(this.saveButton);
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.typeLabel);
+			this.Controls.Add(this.passwordLabel);
 			this.Controls.Add(this.hostLabel);
 			this.Controls.Add(this.portLabel);
 			this.Controls.Add(this.typeComboBox);
 			this.Controls.Add(this.portTextBox);
+			this.Controls.Add(this.passwordTextBox);
 			this.Controls.Add(this.hostTextBox);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
@@ -154,5 +175,7 @@
 		private System.Windows.Forms.Label typeLabel;
 		private System.Windows.Forms.Button cancelButton;
 		private System.Windows.Forms.Button saveButton;
+		private System.Windows.Forms.Label passwordLabel;
+		private System.Windows.Forms.TextBox passwordTextBox;
 	}
 }
