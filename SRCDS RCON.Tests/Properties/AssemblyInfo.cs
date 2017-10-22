e6a1eb09eab5c3a5506c039cf0418de8091c72e8
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-22 16:06:04 -0700

    lets just use normal assembly versions

--- SRCDS RCON.Tests/Properties/AssemblyInfo.cs
+++ SRCDS RCON.Tests/Properties/AssemblyInfo.cs
@@ -14,3 +14,20 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2afff555-6dad-490c-a30f-dbfac5d6f9fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// is the version for the test assembly really important? no?
+// I didn't think so.
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+
